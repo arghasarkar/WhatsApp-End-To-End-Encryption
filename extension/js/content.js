8ac548760dbc79b75110614f17386f3f140ad8dc
Ilias Akhmedov <mrnoone38@gmail.com>
2017-04-01 15:51:54 +0100

    Fixed the string

--- extension/js/content.js
+++ extension/js/content.js
@@ -2,15 +2,9 @@ var messageHolderElements = document.getElementsByClassName("message-text");
 var messages = [];
 var names = [];
 for (i = 0; i < messageHolderElements.length; i++) {
-  var line = document.getElementsByClassName("message-text")[i].children[0].innerText;
-  var name = line.split("] ");
-  var res = name[i].split(": ")[0];
-  console.log(res);
+ var line = document.getElementsByClassName("message-text")[i].children[0].innerText;
+  var name = line.split("]")[1];
+  var res = name.split(":")[0];
   names.push(res);
   messages.push(document.getElementsByClassName("message-text")[i].children[1].innerText);
 }
-
-for (var i = 0; i < messages.length; i++) {
-  console.log(names[i] + " " + messages[i]);
-}
-//document.getElementsByClassName("message-text")[i].children[0].innerText
