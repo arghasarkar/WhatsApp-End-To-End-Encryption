ed5c66324131e26a0cb16a3658392b7151a25ba1
Argha Sarkar <argha@arghasarkar.co.uk>
2017-04-01 17:04:35 +0100

    feat(ext): Add key generate in popup (#2)
    - Able to generate private and public keys

---
+++ extension/js/popup.js
@@ -0,0 +1,85 @@
+const PASSPHRASE = "mlhphrime2017@teamalpha";
+
+// Adding event listeners to listen to key presses on gen new key and fetch key
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("genNewKey").addEventListener('click', updateCurrentUser);
+    document.getElementById("fetchKey").addEventListener('click', fetchKey);
+});
+
+let key = {
+    id: -1,
+    name: "",
+    private_key: "",
+    public_key: ""
+};
+
+let user = {
+    id: 1,
+    full_name: "Argha",
+    email: "argha.sarkar1994@gmail.com",
+    phone_number: "+447554164303",
+    keys: []
+};
+
+/**
+ * Stores the user details to localstorage
+ */
+function updateCurrentUser() {
+    "use strict";
+
+    generateNewKey().then((key) => {
+        console.log(user);
+    });
+
+    /*chrome.runtime.sendMessage({updateUser: user},function(response){
+
+    });
+    chrome.runtime.onMessage.addListener(function(message,sender,sendResponse) {
+        let str = JSON.stringify(message.data);
+    });*/
+}
+
+function generateNewKey() {
+    "use strict";
+    console.log("Gen new key");
+
+    let options = {
+        userIds: [{ name: user.full_name, email: user.email }], // multiple user IDs
+        numBits: 2048,                                            // RSA key size
+        passphrase: PASSPHRASE       // protects the private key
+    };
+
+    return openpgp.generateKey(options).then(function(key) {
+
+        let generatedKey = [];
+        generatedKey.private_key = key.privateKeyArmored; // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
+        generatedKey.public_key = key.publicKeyArmored;   // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
+
+        user.keys.push(generatedKey);
+
+        return generatedKey;
+    });
+}
+
+function fetchKey() {
+    "use strict";
+    console.log("Fetch an existing key");
+    alert("Fetch Key");
+}
+
+
+
+if (typeof(Storage) !== "undefined") {
+
+    const loggedInUser = "loggedInUser";
+
+    localStorage.setItem(loggedInUser, JSON.stringify(user));
+
+    // Retrieve
+    if (localStorage.getItem(loggedInUser) === null) {
+        console.log("No private key");
+    } else {
+        console.log("Private key found");
+        console.log(localStorage.getItem(loggedInUser));
+    }
+}
