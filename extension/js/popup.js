effd1aadf2bf344785feeaee12c1fe88d8e4a705
Argha Sarkar <argha.sarkar1994@gmail.com>
2017-04-01 16:20:26 +0100

    WIP

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,16 +1,53 @@
-console.log("Printing for popup");
+const PASSPHRASE = "mlhphrime2017@teamalpha";
+
+// Adding event listeners to listen to key presses on gen new key and fetch key
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("genNewKey").addEventListener('click', generateNewKey);
+    document.getElementById("fetchKey").addEventListener('click', fetchKey);
+});
+
+
+function generateNewKey() {
+    "use strict";
+    console.log("Gen new key");
+
+    var options = {
+        userIds: [{ name:'Jon Smith', email:'jon@example.com' }], // multiple user IDs
+        numBits: 4096,                                            // RSA key size
+        passphrase: PASSPHRASE       // protects the private key
+    };
+
+    openpgp.generateKey(options).then(function(key) {
+        var privkey = key.privateKeyArmored; // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
+        var pubkey = key.publicKeyArmored;   // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
+
+        console.log(privkey);
+        console.log(pubkey);
+    });
+}
+
+function fetchKey() {
+    "use strict";
+    console.log("Fetch an existing key");
+    alert("Fetch Key");
+}
+
+let key = {
+    name: "",
+    key: "",
+};
 
 if (typeof(Storage) !== "undefined") {
 
-    const FIELD_PRIVATE_KEY = "privateKey";
+    const loggedInUser = "loggedInUser";
 
-    localStorage.setItem(FIELD_PRIVATE_KEY, "SOME KEY");
+    localStorage.setItem(loggedInUser, JSON.stringify(user));
 
     // Retrieve
-    if (localStorage.getItem(FIELD_PRIVATE_KEY) === null) {
+    if (localStorage.getItem(loggedInUser) === null) {
         console.log("No private key");
     } else {
         console.log("Private key found");
-        console.log(localStorage.getItem(FIELD_PRIVATE_KEY));
+        console.log(localStorage.getItem(loggedInUser));
     }
 }
