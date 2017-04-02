d19edc89f06bcd0a83db4dcdd2170b00cd18ae7a
xavo95 <xavier.perarnau.font@estudiant.upc.edu>
2017-04-02 03:55:53 +0200

    Add fetch support for login

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,21 +1,41 @@
-$(function() {
-    $('form').submit(function() {
+var root_url = 'https://mlhprime2017.herokuapp.com/api/keys/';
 
-        $.post('PageB.php', {
-            'email': $('#inputEmail').val(),
-            'password': $('#inputPassword').val()
-        }, function (data) {
+function composeBody(parameters) {
+    var formBody = [];
+    for (var property in parameters) {
+        var encodedKey = encodeURIComponent(property);
+        var encodedValue = encodeURIComponent(parameters[property]);
+        formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+    return formBody;
+}
 
-            if (data == null) {
-                alert("Error: wrong email or password");
-            } else {
-                //Go to Page C if Page B was successful
-                window.location.href = "index.html";
-            }
-        });
-        return false;
-    })
-});
+function loginChecker() {
+    var url = root_url + 'get_id';
+
+    var params = {
+        email: document.getElementById("inputEmail").value,
+        password: document.getElementById("inputPassword").value
+    };
+
+    var formBody = composeBody(params);
+
+    var request = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        body: formBody
+    };
+
+    fetch(url, request).then(function (res) {
+        return res.json();
+    }).then(function (json) {
+        console.log(json);
+    });
+}
 
 
 // var data = null;
