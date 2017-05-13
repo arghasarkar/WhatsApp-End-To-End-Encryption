c9ee570ca2a4bee9f7822eb2791c6edd743747ad
Argha Sarkar <argha.sarkar1994@gmail.com>
2017-05-13 11:58:34 +0100

    WIP: Added the generateKey function

--- extension/js/content.js
+++ extension/js/content.js
@@ -243,6 +243,27 @@ function fetchLoggedInUser() {
 }
 
 
+function generateNewKey() {
+    "use strict";
+    console.log("Gen new key");
+
+    let options = {
+        userIds: "",     // multiple user IDs
+        numBits: KEY_SIZE,                                          // RSA key size
+        passphrase: PASSPHRASE                                      // protects the private key
+    };
+
+    return openpgp.generateKey(options).then(function(key) {
+
+        let generatedKey = {};
+        generatedKey.privateKey = key.privateKeyArmored; // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
+        generatedKey.publickey = key.publicKeyArmored;   // '-----BEGIN PGP PUBLIC KEY BLOCK ... 'K
+        generatedKey.options = options;
+
+        return generatedKey;
+    });
+}
+
 function encrypt() {
     let options, encrypted;
 
